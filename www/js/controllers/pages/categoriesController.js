6132efa6f95afe5f2e1f729defae3bfdb2730d0b
m <mathsec53@gmail.com>
2025-05-20 03:21:43 -0300

    fix-cleaner-input/init-phrases/sem_frases-text-add-in-cardCategories/enhanced-css-animation

--- www/js/controllers/pages/categoriesController.js
+++ www/js/controllers/pages/categoriesController.js
@@ -1,16 +1,16 @@
-angular.module('atendeFacil').controller('categoriesController', ['$scope', '$timeout', 'localDatabase', '$location', function ($scope, $timeout, localDatabase, $location) {
+angular.module('atendeFacil').controller('categoriesController', ['$scope', 'localDatabase', '$location', function ($scope, localDatabase, $location) {
     // Inicialização
     localDatabase.initialData() // cria caso n exista o banco de dados
     $scope.data = localDatabase.getData()
     $scope.categories = $scope.data.categories
     $scope.phrases = $scope.data.phrases
 
+    //######################## go to phrases ############################################
     $scope.goToPhrases = function (categoryId) {
         $location.path('/phrases/' + categoryId)
     }
+
     //######################## CARDS CATEGORY ############################################
-
-
     $scope.removeCategory = function (categoryId, $event) {
         $event.stopPropagation() // impede de clicar no card 
         if (confirm('Tem certeza que deseja remover esta categoria e todas suas frases?')) {
@@ -27,11 +27,10 @@ angular.module('atendeFacil').controller('categoriesController', ['$scope', '$ti
         $scope.phrases = $scope.data.phrases //atualiza frases
         let modal = bootstrap.Modal.getInstance(document.getElementById('modalCategories')) // seta modal na variavel
         modal.hide() // fecha modal
-        $scope.resetModal()
     }
-    $scope.resetModal = function () {     // reset input modalCategory ao fechar com delay
-        $timeout(function () {
+    $scope.resetModal = function () {     // reset input modalCategory ao fechar
+        if (!$scope.newCategoryName == "") {
             $scope.newCategoryName = ""
-        }, 200)
+        }
     }
 }])
--- www/js/controllers/pages/phrasesController.js
+++ www/js/controllers/pages/phrasesController.js
@@ -1,2 +1,14 @@
-app.controller('phrasesController', function($scope) {
-})
+angular.module('atendeFacil').controller('phrasesController', ['$scope', '$routeParams', '$location', 'localDatabase', function ($scope, $routeParams, $location, localDatabase) {
+
+    // Inicializa os dados
+    $scope.categoryId = parseInt($routeParams.categoryId) // converte o id da url para número (ex: '/phrases/123' → 123)
+    $scope.data = localDatabase.getData() // pega todos os dados salvos no localstorage
+    $scope.categories = $scope.data.categories // armazena a lista de categorias
+    $scope.currentCategory = $scope.categories.find(object => object.id === $scope.categoryId) // encontra a categoria atual pelo id
+    $scope.phrases = $scope.data.phrases[$scope.categoryId] || [] // pega a lista de frases da categoria atual ou usa array vazio se não existir
+
+    //######################## go to main menu ############################################
+    $scope.goBack = function () {
+        $location.path('/')
+    }
+}])
